f42da774aa36ffdda7af527d1624e9f6f60ef6ac
Miri Mendes <108680161+miriamrms@users.noreply.github.com>
2025-01-31 17:31:32 -0300

    Update MultiplicacaoCompleto.asm

--- MultiplicacaoCompleto.asm
+++ MultiplicacaoCompleto.asm
@@ -1,21 +1,21 @@
 #==================================
-#Obs: Para o input, digite um sinal (+ ou -) seguido de um numero ate 5 digitos, um espaço e mais outro sinal seguido de outro num de 5 digitos
+#Obs: Para o input, digite um sinal (+ ou -) seguido de um numero ate 5 digitos, um espaÃ§o e mais outro sinal seguido de outro num de 5 digitos
 #Exemplo: +2416 -12
 #=================================
 
 #inicializar valores para multiplicacao
 add x10, x10, x0        			 #multiplicando
-add x11, x11, x0         		 #multiplicador
-addi x13, x13, 17 							#contador
-addi x12, x12, 0							#produto
-addi x14, x14, 0 							#sinal
+add x11, x11, x0         		   #multiplicador
+addi x13, x13, 17 						 #contador
+addi x12, x12, 0							 #produto
+addi x14, x14, 0 							 #sinal
 
-#cria um vetor que armazena as potências de 10 até 10^9 pois o resultado pode ser até 10 digitos 
+#cria um vetor que armazena as potÃªncias de 10 atÃ© 10^9 pois o resultado pode ser atÃ© 10 digitos 
 #utiliza o lui para armazenar nos 20 primeiros bits
 #utiliza o add para armazenar nos 12 bits menos significativos
-#lui só armazena numeros de até 20 bits
-#add só armazena numeros de até 12 bits
-#precisamos dos dois pois não conseguimos armazenar números maiores que seu limite diretamente
+#lui sÃ³ armazena numeros de atÃ© 20 bits
+#add sÃ³ armazena numeros de atÃ© 12 bits
+#precisamos dos dois pois nÃ£o conseguimos armazenar nÃºmeros maiores que seu limite diretamente
 
 lui x5, 0x3B9AC 						#10^9     
 addi x5, x5, 0xA00						
@@ -55,29 +55,29 @@ sw   x5, 36(x9)
 
 
 #inicializar valores para leitura
-addi x5, x0, 0					#conta quantos números foram lidos
+addi x5, x0, 0					#conta quantos nÃºmeros foram lidos
 add x30, x30, x0
 add x31, x31, x0
-addi x28, x0, 32 			#valor do espaco em ASCII
-addi x29, x0, 45 			#valor de '-' em ASCII
+addi x28, x0, 32 			  #valor do espaco em ASCII
+addi x29, x0, 45 			  #valor de '-' em ASCII
 
 #Ler input do teclado
 lerSinal:
 add x15, x0, x0
-lb x7, 1025(x0)							#x7 guarda o valor do input
+lb x7, 1025(x0)							  #x7 guarda o valor do input
 beq x7, x29, sinalNegativo		#se sinal = '-'
 jal x1, lerNum
 
 sinalNegativo:							
-xori x14, x14, 1							#se é negativo muda o sinal
+xori x14, x14, 1							#se Ã© negativo muda o sinal
 jal x1, lerNum
 
 lerNum:
-lb x7, 1025(x0)							#lê input do teclado
-beq x7, x28, pararInput				 #se for espaço para de ler
-beq x7, x0, pararInput					 #se não tiver mais entrada para de ler também
+lb x7, 1025(x0)							  #lÃª input do teclado
+beq x7, x28, pararInput				#se for espaÃ§o para de ler
+beq x7, x0, pararInput				#se nÃ£o tiver mais entrada para de ler tambÃ©m
 
-addi x7, x7, -48							#transforma de ASCII para número
+addi x7, x7, -48							#transforma de ASCII para nÃºmero
 slli x30, x15, 1							#multiplica por 2
 slli x31, x15, 3							#multiplica por 8
 add x15, x30, x31							#x15 = x15 x (2+8) -> multiplica por 10
@@ -85,71 +85,71 @@ add x15, x15, x7							#adiciona a nova entrada na unidade
 jal x1, lerNum
 
 pararInput:
-beq x5, x0, criarMultiplicando		#se é o primeiro num -> add em x10, multiplicando
-add x11, x11, x15 							   	#senão, add em x11, multiplicador
+beq x5, x0, criarMultiplicando		  #se Ã© o primeiro num -> add em x10, multiplicando
+add x11, x11, x15 							   	#senÃ£o, add em x11, multiplicador
 jal x1, loop
 
 criarMultiplicando:
 add x10, x10, x15 
 addi x5, x5, 1
-jal x1, lerSinal								#volta para ler o segundo número
+jal x1, lerSinal								    #volta para ler o segundo nÃºmero
 
 #loop da multiplicacao 
 loop:
-andi x5, x11, 1								#bit menos significativo do multiplicador em x5
-beq x5, x0, deslocamento								#se x5 < 0, deslocamento 
+andi x5, x11, 1								      #bit menos significativo do multiplicador em x5
+beq x5, x0, deslocamento						#se x5 < 0, deslocamento 
 jal x1, multiplicadorIgualUm				
 
 #funcao caso o menos significativo seja 1
 multiplicadorIgualUm:
-add x12, x12, x10							#adiciona o multiplicando no produto	
+add x12, x12, x10							      #adiciona o multiplicando no produto	
 jal x1, deslocamento
 
 deslocamento: 
-slli x10, x10, 1  							#desloca multiplicando a esquerda
-srli x11, x11, 1 							#desloca multiplicador a direita
-addi x13, x13, -1 							#decrementa o contador
-beq x13, x0, imprimirSinal		#se o contador < 0 , imprime resultado
-jal x1, loop							#se nao, reinicia o loop
+slli x10, x10, 1  							  #desloca multiplicando a esquerda
+srli x11, x11, 1 							    #desloca multiplicador a direita
+addi x13, x13, -1 							  #decrementa o contador
+beq x13, x0, imprimirSinal		    #se o contador < 0 , imprime resultado
+jal x1, loop							        #se nao, reinicia o loop
 
 
 imprimirSinal:
-beq x14, x0, imprimirPositivo		#se x14 = 0, sinal é positivo
-addi x14, x0, 45							   #senão, x14 recebe 45 (valor que representa '-')
+beq x14, x0, imprimirPositivo		 #se x14 = 0, sinal Ã© positivo
+addi x14, x0, 45							   #senÃ£o, x14 recebe 45 (valor que representa '-')
 sb x14, 1024(x0)							   #imprime o sinal negativo
 jal x1, imprimirNum						
 
 imprimirPositivo:
-addi x14, x0, 43						 #x14 = 43 (valor de '+')
-sb x14, 1024(x0)						 #imprime sinal +
+addi x14, x0, 43						   #x14 = 43 (valor de '+')
+sb x14, 1024(x0)						   #imprime sinal +
 jal x1, imprimirNum
 
 imprimirNum:
-addi x5, x0, 0						#contador digitos lidos
-addi x6, x0, 10						#quantidade maxima de digitos
-addi x20, x12, 0						#salva o resultador
+addi x5, x0, 0						    #contador digitos lidos
+addi x6, x0, 10						    #quantidade maxima de digitos
+addi x20, x12, 0						  #salva o resultador
 
 loopImprimir:
-bge x5, x6, end						#se x5>=10 todos os dígitos foram impressos
-slli x7, x5, 2 						#multiplica por 4 e guarda em x7 para acessar o espaço da memória
-add x7, x7, x9						#soma o endereco da potência de 10 correspondente com base no array
-lw x7, 0(x7)						#armazena o valor da potência de 10
+bge x5, x6, end						  #se x5>=10 todos os dÃ­gitos foram impressos
+slli x7, x5, 2 						  #multiplica por 4 e guarda em x7 para acessar o espaÃ§o da memÃ³ria
+add x7, x7, x9						  #soma o endereco da potÃªncia de 10 correspondente com base no array
+lw x7, 0(x7)						    #armazena o valor da potÃªncia de 10
 add x28, x0, x0						
 
 dividirPotencia:
-blt x20, x7, imprimirDigito		#x20 < potencia de 10? sim -> próximo digito
-sub x20, x20, x7							#senao, subtrai a potência de 10 o quanto puder
-addi x28, x28, 1							#soma 1 a cada subtracao, até encontrar o digito atual
-jal x0, dividirPotencia     #chama a funcao novamente
+blt x20, x7, imprimirDigito		  #x20 < potencia de 10? sim -> prÃ³ximo digito
+sub x20, x20, x7							  #senao, subtrai a potÃªncia de 10 o quanto puder
+addi x28, x28, 1							  #soma 1 a cada subtracao, atÃ© encontrar o digito atual
+jal x0, dividirPotencia         #chama a funcao novamente
 
 imprimirDigito:
-blt x12, x7, continuar						#se o valor < que a potência de 10 pula para não imprimir os zeros a esquerda
-addi x28, x28, 48							#converte para caractere
-sb x28, 1024(x0)							#imprime o digito
+blt x12, x7, continuar					#se o valor < que a potÃªncia de 10 pula para nÃ£o imprimir os zeros a esquerda
+addi x28, x28, 48							  #converte para caractere
+sb x28, 1024(x0)							  #imprime o digito
 
 continuar:
-addi x5, x5, 1							#incrementa o contador de digitos
-jal x0, loopImprimir							#imprimir o proximo digito
+addi x5, x5, 1							    #incrementa o contador de digitos
+jal x0, loopImprimir						#imprimir o proximo digito
 
 end:
 halt
